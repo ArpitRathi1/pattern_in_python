397ccadf6188b184f3503d0594b219d9923eee40
ArpitRathi1 <rathi844@gmail.com>
2023-12-28 12:26:56 +0530

    added pattern_3

---
+++ pattern_3.py
@@ -0,0 +1,13 @@
+# Ques) Print the given pattern.
+# for n=3      for n=4
+# *            *
+# **           **
+# ***          ***
+#              ****
+# Solution-
+
+n=int(input("Enter n:"))
+for i in range(1,(n+1)):
+	for j in range(1,(i+1)):
+		print("*",end="")
+	print()
