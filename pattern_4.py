b879094cf9579af16aa30061dff0a86acb21bc31
ArpitRathi1 <rathi844@gmail.com>
2023-12-29 10:16:46 +0530

    added pattern 4

---
+++ pattern_4.py
@@ -0,0 +1,12 @@
+# Ques) Print the given pattern.
+# for n=3       for n=4
+# 1             1
+# 12            12
+# 123           124
+#               1234
+
+n=int(input("Enter n:"))
+for i in range(1,(n+1)):
+	for j in range(1,(i+1)):
+		print(j,end="")
+	print()
