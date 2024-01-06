8874271232d7a8d83c31a34d6a28c0ab8eb6b0e3
ArpitRathi1 <rathi844@gmail.com>
2024-01-06 08:51:20 +0530

    added pattern 16

---
+++ pattern_16.py
@@ -0,0 +1,14 @@
+# Ques) print the given pattern.
+# for n=4
+#    *
+#   ***
+#  *****
+# *******
+# Solution-
+
+n=int(input("Enter n : "))
+for i in range (1,(n+1)):
+	print(" " * (n-i),end="")
+	for j in range (1,2*i-1+1):
+		print("*",end="")
+	print()
